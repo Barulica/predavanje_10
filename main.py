892658be2dea9dcee2d46575dee0fb493789b64d
Barulica <barulabarula@gmail.com>
2024-12-25 13:02:44 +0100

    made main structure and created empty products.json file

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import json
 
-def test (whats_up)
+with open("user.json", 'r') as file:
+    data = json.load(file)
+
+print(data[1]['name'])
+
+with open("user.json", 'w') as file:
+    json.dump(data, file, indent=4)
+    data.append({'name': 'Tolima', 'age': 25, "height": 160, "gender": "male"})
+
+print(data)
+
+
