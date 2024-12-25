358a7661403ecdb732f0e3a7ca1076b416aeb5a2
Barulica <barulabarula@gmail.com>
2024-12-25 13:08:50 +0100

    made main structure and created empty products.json file

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import json
 
-with open("user.json", 'r') as file:
+with open("data/users.json", 'r') as file:
     data = json.load(file)
 
 print(data[1]['name'])
 
-with open("user.json", 'w') as file:
+with open("data/users.json", 'w') as file:
     json.dump(data, file, indent=4)
     data.append({'name': 'Tolima', 'age': 25, "height": 160, "gender": "male"})
 
 print(data)
 
+with open("data/products.json", 'r') as file:
+    data = json.load(file)
+print(data)
 
