6bf807e8effa7da8f833896198698362a88671db
Barulica <barulabarula@gmail.com>
2024-12-26 15:29:57 +0100

    made main structure and created empty products.json file

--- 11/firstproject.py
+++ 11/firstproject.py
@@ -17,10 +17,48 @@ if maximum_budget >= limit_budget or maximum_budget <= 0:
 
 print(f"Welcome back, {user['name']}! Your budget is {maximum_budget}")
 
-expense = 0
+def clear_logs():
+    with open('exercise/logs/expense.txt', 'w') as file:
+        pass
+    print("Logs cleared")
 
-while expense <= 0 or expense >= maximum_budget:
-    expense = int(input("Enter expense: \n"))
 
-budget_after_expense = maximum_budget - expense
-print(f"{user['name']}, your budget is now {budget_after_expense}")
+while True:
+    print("\nMenu:")
+    print("1. Enter an expense")
+    print("2. Clear logs")
+    print("3. Exit")
+
+    choice = input("Choose an option: \n")
+
+    if choice == '1':
+        try:
+            expense = int(input("Enter expense: \n"))
+
+            if 0 < expense <= maximum_budget:
+
+                maximum_budget -= expense
+                budget_after_expense = maximum_budget
+
+                print(f"{user['name']}, your budget is now {budget_after_expense}")
+
+
+                with open('exercise/logs/expense.txt', 'a') as file:
+                    message = f"Amount of expense: {expense}, User: {user['name']}, Budget: {budget_after_expense}\n"
+                    file.write(message)
+
+            else:
+                print("You don't have enough budget")
+
+        except ValueError:
+            print("Not valid input")
+
+    elif choice == '2':
+        clear_logs()
+
+    elif choice == '3':
+        print("Goodbye.")
+        break
+
+    else:
+        print("Invalid choice. Please select a valid option.")
